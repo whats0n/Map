57fa748227b00ec8e83c8bf91390942c2e016d16
whats0n <mrdvv@mail.ru>
2017-02-22 17:29:03 +0200

    update

---
+++ src/js/app.js
@@ -0,0 +1,79 @@
+import Map from './_map';
+
+let map = new Map({
+	infobox: {
+		class: 'awesome-infobox',
+		style: {
+			width: '300px'
+		},
+		position: {
+			y: "center",
+			x: "left"
+		},
+		closeButton: '.js-infobox-close'
+	},
+	onlyOneBox: true,
+	template: '#infobox',
+	container: '.js-map',
+	options: {
+		center: {lat: -34.097, lng: 150.644},
+		zoom: 8
+	},
+	markers: {
+		ajax: true,
+		file: 'data-file',
+	}
+	// markers: {
+	// 	ajax: false,
+	// 	items: [
+	// 		{
+	// 			"content": {
+	// 				"title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, consequatur."
+	// 			},
+	// 			"marker": {
+	// 				"position": {
+	// 					"lat": -34.397,
+	// 					"lng": 150.644
+	// 				},
+	// 				"icon": {
+	// 					"default": "/img/markerDefault.png",
+	// 					"active": "/img/markerDefault.png",
+	// 					"size": {
+	// 						"x": 20,
+	// 						"y": 30
+	// 					},
+	// 					"centering": {
+	// 						"x": 10,
+	// 						"y": 30
+	// 					}
+	// 				}
+	// 			}
+	// 		},
+	// 		{
+	// 			"content": {
+	// 				"title": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, consequatur."
+	// 			},
+	// 			"marker": {
+	// 				"position": {
+	// 					"lat": -34.597,
+	// 					"lng": 150.644
+	// 				},
+	// 				"icon": {
+	// 					"default": "/img/markerDefault.png",
+	// 					"active": "/img/markerDefault.png",
+	// 					"size": {
+	// 						"x": 20,
+	// 						"y": 30
+	// 					},
+	// 					"centering": {
+	// 						"x": 10,
+	// 						"y": 30
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	]
+	// }
+});
+
+map.init();
